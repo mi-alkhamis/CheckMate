2056a13eca8ec409ad13c6642ff643baebcb93fd
Milad Alkhamis <mi-alkhamis@hotmail.com>
2023-08-14 19:24:54 +0330

    create creat a new user

--- user.py
+++ user.py
@@ -1,7 +1,24 @@
-from models import  users_schema, User
+from flask import abort
+from config import db
+from models import user_schema, users_schema, User
 
 
 def read_all():
     all_users = User.query.all()
     return users_schema.dump(all_users)
+
+
+def create(user):
+   username = user.get('username')
+   existing_user = User.query.filter(User.username == username).one_or_none()
+
+   if existing_user is None:
+        new_user = user_schema.load(user ,session=db.session)
+        db.session.add(new_user)
+        db.session.commit()
+        return user_schema.dump(new_user), 201
+   else:
+        abort(
+            406, f"Username {username}  already exist."
+        )
 
